7e1176fdbda78febb310f55727b2b1130133c322
Yevhen <krenichniy1995@gmail.com>
2022-06-12 11:17:08 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './scss/main.scss';
 import './css/main.css';
+
+import { APIService } from './js/services/APIService';
+
+const newPokemonApi = new APIService();
--- src/js/services/APIservice.js
+++ src/js/services/APIservice.js
@@ -1,10 +1,28 @@
 import axios from 'axios';
 
-const BASE_URL = '';
+const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
 
 export class APIService {
   constructor() {
     this.query = '';
     this.url = '';
+  }
+
+  getPokemonsList() {
+    return fetch(BASE_URL).then(response => {
+      if (!response.ok) {
+        throw new Error();
+      }
+      return response.json().results;
+    });
+  }
+
+  getPokemon(pokemon) {
+    return fetch(`${BASE_URL}/${pokemon}`).then(response => {
+      if (!response.ok) {
+        throw new Error();
+      }
+      return response.json();
+    });
   }
 }
